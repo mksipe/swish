9145d62cb6157221e9095f7d7987347d570e9cfd
Mason Sipe <masonsipe0513@gmail.com>
2019-12-12 20:43:18 +0000

    update requirements

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import glob, sys, configparser, subprocess
+import glob, configparser
 confdir = "./config/*/*.dat"
 parser = configparser.ConfigParser()
 config = configparser.RawConfigParser(allow_no_value=True)
