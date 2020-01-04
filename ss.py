1c9afa14807063772686559e58d0b1329ebb582f
Mason Sipe <masonsipe0513@gmail.com>
2020-01-04 02:43:12 +0000

    ss close to done.

--- ss.py
+++ ss.py
@@ -1,23 +1,37 @@
-import subprocess
-print("Fetching remote modules ... ")
+import subprocess, configparser
+parser = configparser.ConfigParser()
 module_dir = "https://github.com/mksipe/swish-modules/trunk/dat/"
 class options:
   def list():
     subprocess.call(['svn','ls', module_dir], shell=False)
   def info(module):
-    subprocess.call(['svn', 'info', module], Shell=False)
+    subprocess.call(['svn', 'cat', 'https://github.com/mksipe/swish-modules/trunk/dat/'+module], shell=False)
   def fetch(module):
-    subprocess.call(['svn', 'export', module], shell=False)
+    subprocess.call(['svn', 'export', 'https://github.com/mksipe/swish-modules/trunk/dat/'+module], shell=False)
 
 from cmd import Cmd
  
 class MyPrompt(Cmd):
-   def do_exit(self, inp):
+  prompt = 'SS> '
+  intro = "swish store - Type ? to list commands"
+
+  def do_exit(self, inp):
         print("Bye")
         return True
- 
-   def list(self, inp):
-        options.list().format(inp)
- 
+  def do_list(self, inp):
+        options.list()
+  def do_info(self, inp):
+        txt = input("SS[Search]>")
+        options.info(txt)
+  def do_install(self, inp):
+        txt = input("SS[Install]>")
+        subprocess.call('cd config/modules/')
+        options.fetch(txt)
+        parser.read(txt)
+        parser.sections()
+        asc = parser.get("download", 'checksum')
+        run = parser.get("download",'runfile')
+        print(asc)
+        print(run)
 MyPrompt().cmdloop()
 print("after")
