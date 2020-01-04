e384ac3b0cb588a04ec2c19958646b7567d082cd
Mason Sipe <masonsipe0513@gmail.com>
2020-01-04 02:37:31 +0000

    ss system almost works

--- ss.py
+++ ss.py
@@ -8,3 +8,16 @@ class options:
     subprocess.call(['svn', 'info', module], Shell=False)
   def fetch(module):
     subprocess.call(['svn', 'export', module], shell=False)
+
+from cmd import Cmd
+ 
+class MyPrompt(Cmd):
+   def do_exit(self, inp):
+        print("Bye")
+        return True
+ 
+   def list(self, inp):
+        options.list().format(inp)
+ 
+MyPrompt().cmdloop()
+print("after")
