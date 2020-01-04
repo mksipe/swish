389cd4a2becdafbf811bd81d26415cc0ae3b1de9
Mason Sipe <masonsipe0513@gmail.com>
2020-01-04 02:55:26 +0000

    ss test

--- ss.py
+++ ss.py
@@ -8,6 +8,8 @@ class options:
     subprocess.call(['svn', 'cat', 'https://github.com/mksipe/swish-modules/trunk/dat/'+module], shell=False)
   def fetch(module):
     subprocess.call(['svn', 'export', 'https://github.com/mksipe/swish-modules/trunk/dat/'+module], shell=False)
+  def subfetch(module):
+      subprocess.call(['svn', 'export', module], shell=False)
 
 from cmd import Cmd
  
@@ -25,13 +27,17 @@ class MyPrompt(Cmd):
         options.info(txt)
   def do_install(self, inp):
         txt = input("SS[Install]>")
-        subprocess.call('cd config/modules/')
+        subprocess.call(["cd config/modules/"], shell=False)
         options.fetch(txt)
         parser.read(txt)
         parser.sections()
         asc = parser.get("download", 'checksum')
         run = parser.get("download",'runfile')
-        print(asc)
-        print(run)
+        subprocess.call(["cd run/"], shell=False)
+        options.subfetch(run)
+        subprocess.call(["cd ../../tmp"], shell=False)
+        options.subfetch(asc)
+        subprocess.call(["cat", txt.strip(".dat")+".asc", ">>", "../config/hashes.asc" ], shell=False)
+
 MyPrompt().cmdloop()
 print("after")
